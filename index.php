a7e26987feccdd15702faf53c142d2324d0e114e
camila062 <camilalidiaaraujo@gmail.com>
2020-10-15 14:20:26 -0300

    Alteração

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     sqahbssbdfff
     <h3>ALTERAÇÃO 1</h3>
-
+    <h2>ALTERAÇÃO 2</h2>
     <H1>EVELINDA</H1>
 </body>
+
 </html>
